a7035ab6c2f7d5eac44bac7e6cd85d76019ef663
Ethan Miller <ethanmillercsci@gmail.com>
2019-11-04 16:34:47 -0700

    Add files via upload

---
+++ demolamp/header.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+
+$clubstr = 'Jamming in the Bozone Layer';
+$userstr = 'Welcome Guest';
+
+echo <<<_INIT
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <script src='javascript.js'></script>
+        <link href="https://fonts.googleapis.com/css?family=Arsenal|Lora|Muli|Source+Sans+Pro|Playfair+Display&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Bowlby+One+SC&display=swap" rel="stylesheet">
+        <link rel='stylesheet' href='css/styles.css'>
+        <title>$clubstr: $userstr</title>
+        </head>
+_INIT;
+
+require_once 'functions.php';
+
+if (isset($_SESSION['user'])) {
+    $user     = $_SESSION['user'];
+    $loggedin = TRUE;
+    $userstr  = "Logged in as: $user";
+}
+else $loggedin = FALSE;
+
+echo <<<_HEADER_OPEN
+
+    <body>
+        <div id="wrapper">
+        <header>
+            <div id='logo'>$clubstr</div>
+            <div class='username'>$userstr</div>
+_HEADER_OPEN;
+
+if ($loggedin) {
+echo <<<_LOGGEDIN
+
+            <nav><ul>
+                <li><a href='members.php?view=$user'>Home</a></li>
+                <li><a href='members.php'>Members</a></li>
+                <li><a href='friends.php'>Friends</a></li>
+                <li><a href='messages.php'>Messages</a></li>
+                <li><a href='profile.php'>Edit Profile</a></li>
+                <li><a href='logout.php'>Log out</a></li>
+                <li><a href='featured.php'>Featured Artist</a></li>
+            </ul></nav>
+_LOGGEDIN;
+} else {
+echo <<<_GUEST
+
+            <nav><ul>
+                <li><a href='index.php'>Home</a></li>
+                <li><a href='signup.php'>Sign Up</a></li>
+                <li><a href='login.php'>Log In</a></li>
+                <li><a href='featured.php'>Featured Artist</a></li>
+            </ul></nav>
+_GUEST;
+ }
+
+echo <<<_HEADER_CLOSE
+
+        </header>
+
+        <div id="content">
+_HEADER_CLOSE;
+
+?>
