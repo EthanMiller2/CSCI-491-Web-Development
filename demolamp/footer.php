51c48de87883ad95f04b38632cb56937bbe54e09
bridget.wermers <m89p335@csci491-01.cs.montana.edu>
2019-11-06 15:57:52 -0700

    first push

--- demolamp/footer.php
+++ demolamp/footer.php
@@ -3,11 +3,7 @@
     </div>
         <footer>
             <h3>About this Website</h3>
-            <p>$clubstr is a website intended for the community of Bozeman,
-            MT to be able to discover share and create content for the local music scene.
-            Once  a profile is created users can link with people interested in the same music,
-            send messages privately or publically to other users, or suggest an artist to be added
-            to the calender via form submission. </p>
+            <p>Copyright 2019. All rights Reserved. </p>
             <p>Each week a new artist will be featured on the "Featured Artist" page.</p>
         </footer>
         </div>
