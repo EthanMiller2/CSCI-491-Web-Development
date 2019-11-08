4ae34580219a1b7eefce2e67b3550775dc65138b
bridgetwermers13 <bwerms13@gmail.com>
2019-11-07 23:20:58 -0700

    lots of style changes and content addition to the index page.

--- demolamp/header.php
+++ demolamp/header.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-$clubstr = 'Jamming in the Bozone Layer';
-$userstr = 'Welcome Guest';
+$clubstr = 'Jamming in the Bozone';
+
 
 echo <<<_INIT
 <!DOCTYPE html>
@@ -14,18 +14,13 @@ echo <<<_INIT
         <link href="https://fonts.googleapis.com/css?family=Arsenal|Lora|Muli|Source+Sans+Pro|Playfair+Display&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Bowlby+One+SC&display=swap" rel="stylesheet">
         <link rel='stylesheet' href='css/styles.css'>
-        <title>$clubstr: $userstr</title>
+        <title>$clubstr</title>
         </head>
 _INIT;
 
-require_once 'functions.php';
 
-if (isset($_SESSION['user'])) {
-    $user     = $_SESSION['user'];
-    $loggedin = TRUE;
-    $userstr  = "Logged in as: $user";
-}
-else $loggedin = FALSE;
+
+
 
 echo <<<_HEADER_OPEN
 
@@ -33,33 +28,18 @@ echo <<<_HEADER_OPEN
         <div id="wrapper">
         <header>
             <div id='logo'>$clubstr</div>
-            <div class='username'>$userstr</div>
+            <img id="logoImg" src="bg.jpg">
+
+                     <nav><ul>
+                            <li><a href='index.php'>Home</a></li>
+                            <li><a href='calender.php'>Calender</a></li>
+                            <li><a href='featured.php'>Featured Artist</a></li>
+                            <li><a href=#>Photo Share</a></li>
+                        </ul></nav>
+
+
 _HEADER_OPEN;
 
-if ($loggedin) {
-echo <<<_LOGGEDIN
-
-            <nav><ul>
-                <li><a href='members.php?view=$user'>Home</a></li>
-                <li><a href='members.php'>Members</a></li>
-                <li><a href='friends.php'>Friends</a></li>
-                <li><a href='messages.php'>Messages</a></li>
-                <li><a href='profile.php'>Edit Profile</a></li>
-                <li><a href='logout.php'>Log out</a></li>
-                <li><a href='featured.php'>Featured Artist</a></li>
-            </ul></nav>
-_LOGGEDIN;
-} else {
-echo <<<_GUEST
-
-            <nav><ul>
-                <li><a href='index.php'>Home</a></li>
-                <li><a href='signup.php'>Sign Up</a></li>
-                <li><a href='login.php'>Log In</a></li>
-                <li><a href='featured.php'>Featured Artist</a></li>
-            </ul></nav>
-_GUEST;
- }
 
 echo <<<_HEADER_CLOSE
 
