1a54b1a4fc96a7b31c7ec98f0113e7554cf05d8f
Max Carr <36968069+maxmk12@users.noreply.github.com>
2019-11-08 16:04:55 -0700

    Footer first change

--- demolamp/footer.php
+++ demolamp/footer.php
@@ -2,7 +2,7 @@
   echo <<<_END
     </div>
         <footer>
-            <p>Copyright 2019. All rights Reserved. </p>
+            <p>Copyright 2019. All rights Reserved By Group 4. </p>
             <p>Each week a new artist will be featured on the "Featured Artist" page.</p>
         </footer>
         </div>
