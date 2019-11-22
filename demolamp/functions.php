efd4a9f42f7cb1037d1bd00c0b1f5d74f85ffbd3
bridgetwermers13 <bwerms13@gmail.com>
2019-11-22 14:39:24 -0700

    added some functions back into functions.php in order to save to database 4

--- demolamp/functions.php
+++ demolamp/functions.php
@@ -13,6 +13,21 @@ $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 if ($connection->connect_error)
     die("Fatal Error 1");
 
+function queryMysql($query) {
+    global $connection;
+    $result = $connection->query($query);
+    if (!$result) die("Fatal Error 2");
+    return $result;
+}
+
+function sanitizeString($var){
+    global $connection;
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    if (get_magic_quotes_gpc())
+        $var = stripslashes($var);
+    return $connection->real_escape_string($var);
+}
 
 ?>
 
